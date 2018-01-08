925edb9636655086f9f3be43e82c25d324dc195d
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-08 16:06:53 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,8 @@ numbers = [1,2,3]
 new_numbers = [9,10,16,25]
 def square_array(array)
   array.each do |x|
-    x*x
+    new_array.push(x**2)
   end
-end
+  new_array
 puts square_array(numbers)
 puts square_array(new_numbers)
